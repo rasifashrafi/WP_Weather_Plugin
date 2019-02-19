31e278678e0c611e23ec64bed04222c0f534cf21
rasif <rashrafi@techlaunch.com>
2019-02-19 17:38:24 -0500

    first commit

---
+++ weather.php
@@ -0,0 +1,32 @@
+<div class="container">
+	<h1 class="text">Weather Plugin</h1>
+	<div id="form">
+		<div id="post-body-content">
+			<div id="post-body">
+				<div>
+					<div class="postbox">
+						<h2><span>Enter Your Current Location :</span></h2>
+							<div class="content">
+								<form action="" name="weather_form" method="post">
+									<input type="hidden" name="form_submitted" value="Y">
+									<table class="form-table">
+										<tr>
+											<td>
+												<label for="city">City / Zipcode </label>
+											</td><br />
+											<td>
+												<input name="city" id="city" type="text" value="" class="regular-text" />
+											</td>
+										</tr>
+									</table>
+									<p>
+										<input class="button-primary" type="submit" name="submit" value="Submit" />
+									</p>
+								</form>
+							</div>
+					</div>
+				</div>
+			</div>
+		</div><br />
+	</div>
+</div>
---
+++ weather_view.php
@@ -0,0 +1,103 @@
+<?php
+/*
+*
+Plugin Name: weather plugin
+Plugin URI: https://github.com/rasifasrafi
+Description: This is a  weather plugin
+Author: Rasif Ashrafi
+Version: 1.0.0
+Author URI: http://rashrafi.techlaunch.io
+*
+*/
+$plugin_url = WP_PLUGIN_URL . '/weather';
+function plugin_install() {
+    global $wpdb;
+    return true;
+}
+function option_menu() {
+    add_options_page(
+        'Weather Plugin',
+        'Weather Plugin',
+        'manage_options',
+        'weather-plugin',
+        'option_page'
+    );
+}
+add_action('admin_menu', 'option_menu');
+function option_page() {   
+    
+    if( !current_user_can( 'manage_options' ) ) { 
+        wp_die( 'You don\'t have sufficient permissions to access this page.' );    
+    }
+    global $plugin_url;
+    global $city;
+    
+    if( isset( $_POST['form_submitted'] ) ) {
+        $hidden_field = esc_html( $_POST['form_submitted'] );
+        
+        if( $hidden_field == 'Y' ) {
+            
+            $city = esc_html( $_POST['city'] );
+            
+            $weather =  getWeather($city );
+            
+        }
+        
+    }
+    
+    require('weather.php');
+}
+function getWeather($city){   
+    
+    $json_feed_url = 'https://api.openweathermap.org/data/2.5/weather?q=' . $city . '&appid=b97bb751313b5a72afe4f1be164184c5';
+    
+    $args = array('timeoute' => 120);
+    
+    $json_feed = wp_remote_get( $json_feed_url, $args );
+    
+    $weather_updates = json_decode( $json_feed['body'] );
+?>
+<div class="main">
+    <div class="inside">
+        <article class="weather-plugin">
+            <div class="weatherIcon">
+                <h1 class="head">Local Weather Details : </h1>
+                <div id="icon"><img src="http://openweathermap.org/img/w/<?= $weather_updates->weather[0]->icon?>.png"
+                        alt="Weather icon" /></div>
+                <div class="info">City :
+                    <?= $weather_updates->name . ', ' . $weather_updates->sys->country ?>
+                </div>
+                <div class="info">Temperature :
+                    <?= floor(($weather_updates->main->temp) - 273.15) . '&#8451';?>
+                </div>
+                <div class="info">Min. Temperature :
+                    <?= floor(($weather_updates->main->temp_min) - 273.15) . '&#8451;' ; ?>
+                </div>
+                <div class="info">Max. Temperature :
+                    <?= floor(($weather_updates->main->temp_max) - 273.15) . '&#8451;' ; ?>
+                </div>
+                <div class="info">Description :
+                    <?= $weather_updates->weather[0]->description;?>
+                </div>
+                <?= $weather_updates->main->humidity ; ?> 
+                 <?= $weather_updates->main->pressure ; ?>  
+                 <?= $weather_updates->wind->speed ; ?> 
+            </div>
+        </article>
+    </div>
+</div>
+<?php  
+}
+function plugin_deactivate()
+{
+    global $wpdb;
+    echo "deactivate";
+}
+function weather_styles() {
+	wp_enqueue_style( 'weather_styles', plugins_url( 'weather/style.css' ) );
+}
+add_action( 'admin_head', 'weather_styles' );
+add_action('wp_enqueue_scripts', 'getWeather');
+register_activation_hook(__FILE__, 'plugin_install');
+register_deactivation_hook(__FILE__, 'plugin_deactivate');
+?>
